0b07bf3d9e1f1f58e943424e2cf1880896b7086a
ludovicfourcroy <ludovicfourcroy8@gmail.com>
2020-09-16 20:56:30 +0200

    crud

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,48 @@
 class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat), notice: 'Flat was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    if @flat.update(flat_params)
+      redirect_to flat_path(@flat), notice: 'Flat was successfully update.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :stars)
+  end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,7 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @flat = Flat.find(params[:flat_id])
+    @review = Review.new
+  end
 end
