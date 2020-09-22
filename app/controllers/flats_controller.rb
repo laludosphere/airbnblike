050ec113ad64f8714ad1f1166b9eda8175cc5823
ludovicfourcroy <ludovicfourcroy8@gmail.com>
2020-09-22 17:36:15 +0200

    ok

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -54,6 +54,6 @@ class FlatsController < ApplicationController
   private
 
   def flat_params
-    params.require(:flat).permit(:name, :address, :stars, images: [] )
+    params.require(:flat).permit(:name, :address, :stars)
   end
 end
